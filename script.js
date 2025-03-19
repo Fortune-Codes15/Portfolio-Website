b99cff977e602eabf04fc470bcd12dffeba395cb
Fortune <fortunefoluso26@gmail.com>
2025-03-19 20:59:36 +0100

    Updated File

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ document.addEventListener( "DOMContentLoaded", function () {
 
   elements.forEach( element => observer.observe( element ) );
 } );
+
+const nav_bar = document.querySelector( 'nav' )
+let timeout;
+window.addEventListener( 'scroll', () => {
+  if ( window.scrollY > 10 ) {
+    nav_bar.classList.add( 'fixed' )
+    clearTimeout( timeout );
+  } else {
+    nav_bar.classList.remove( 'fixed' )
+  }
+} );
